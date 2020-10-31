c41f8265616c8b0a995a4af49f3bbc3cb4dbc478
nightspite <we4rpl2@gmail.com>
2020-10-31 18:45:48 +0100

    💡 created get posts function

---
+++ src/views/GetPosts.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+
+const url = `http://localhost:8888/.netlify/functions/getposts`;
+
+export default function GetPosts() {
+  return <div />;
+}
