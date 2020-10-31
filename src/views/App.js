8fde3c8f4f85c1106c00f19f46d284f6f48e31db
nightspite <we4rpl2@gmail.com>
2020-10-31 10:52:48 +0100

    netlify try #3

--- src/views/App.js
+++ src/views/App.js
@@ -1,8 +1,8 @@
-// import React from 'react';
-// // import { getPosts } from 'functions/instagram';
+import React from 'react';
+// import { getPosts } from 'functions/instagram';
 
-// function App() {
-//   return <div className="App">{getPosts}</div>;
-// }
+function App() {
+  return <div className="App">ebe</div>;
+}
 
-// export default App;
+export default App;
