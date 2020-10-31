57cc88ea18358edd453f9bc6fc1499d9016a4394
nightspite <we4rpl2@gmail.com>
2020-10-31 20:26:44 +0100

    📼 display instagram post from profiles

--- functions/getposts.js
+++ functions/getposts.js
@@ -12,26 +12,29 @@ function slimUpPosts(response) {
   return response.data.user.edge_owner_to_timeline_media.edges.map((edge) =>
     edge.node.edge_sidecar_to_children
       ? {
-          image: edge.node.display_url,
           thumbnail: edge.node.thumbnail_src,
           isVideo: edge.node.is_video,
           video: edge.node.video_url,
-          url: `https://instagram.com/p/${edge.node.shortcode}`,
-          id: edge.node.id,
+          image: edge.node.display_url,
+          postUrl: `https://instagram.com/p/${edge.node.shortcode}`,
+          postId: edge.node.id,
+          postShortcode: edge.node.shortcode,
 
           sideImages: edge.node.edge_sidecar_to_children.edges.map((edged) => ({
-            image: edged.node.display_url,
-            thumbnail: edge.node.thumbnail_src,
+            thumbnail: edged.node.display_resources[0].src,
             isVideo: edged.node.is_video,
+            video: edged.node.video_url,
+            image: edged.node.display_url,
           })),
         }
       : {
-          image: edge.node.display_url,
           thumbnail: edge.node.thumbnail_src,
           isVideo: edge.node.is_video,
           video: edge.node.video_url,
-          url: `https://instagram.com/p/${edge.node.shortcode}`,
-          id: edge.node.id,
+          image: edge.node.display_url,
+          postUrl: `https://instagram.com/p/${edge.node.shortcode}`,
+          postId: edge.node.id,
+          postShortcode: edge.node.shortcode,
         },
   );
 }
