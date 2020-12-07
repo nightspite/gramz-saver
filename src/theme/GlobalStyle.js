340715018b11a34077ed8eaeb661a210cbcae371
nightspite <n16h75p173@gmail.com>
2020-12-08 00:43:42 +0100

    💅 added some styling to homepage component

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -5,8 +5,16 @@ const GlobalStyle = createGlobalStyle`${css`
   *::before,
   *::after {
     box-sizing: border-box;
+    outline: none;
+
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none;
   }
   html {
     font-size: 62.5%;
@@ -18,6 +26,7 @@ const GlobalStyle = createGlobalStyle`${css`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
       Helvetica, Arial, sans-serif;
     font-size: 1.4rem;
+    outline: none;
     /* font-size: 1.6rem; */
     /* line-height: 2.4rem; */
   }
