36edebb2a29d53ae3823ad9a9ff0b40479fa1135
nightspite <we4rpl2@gmail.com>
2020-12-09 01:15:48 +0100

    🔧little fixes of styling

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Search from 'components/Search';
 
+const HomeWrapper = styled.div`
+  height: 100vh;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Home = () => (
-  <div>
+  <HomeWrapper>
+    <h2>Search for Instagram Profiles!</h2>
     <Search />
-  </div>
+  </HomeWrapper>
 );
 
 export default Home;
